9f82ddffd924575f6af25e281c23a326210f9ca1
Jakub Sokołowski <jakub@status.im>
2019-05-24 07:40:18 -0400

    Updates

---
+++ precache-manifest.5bebfb15a7f6da0c6893a28ec9f8e6dd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c555cec35a140ec5f951e108b7837c96",
+    "url": "/status-localize/index.html"
+  },
+  {
+    "revision": "5c6f4fb28a343190ecd7",
+    "url": "/status-localize/static/css/main.260a1825.chunk.css"
+  },
+  {
+    "revision": "da0ad5231718e782d358",
+    "url": "/status-localize/static/js/2.409bb0b5.chunk.js"
+  },
+  {
+    "revision": "5c6f4fb28a343190ecd7",
+    "url": "/status-localize/static/js/main.83361708.chunk.js"
+  },
+  {
+    "revision": "0e50cff0b1ac2543689f",
+    "url": "/status-localize/static/js/runtime~main.d8cb264c.js"
+  },
+  {
+    "revision": "c5c7b09c827d98586b1e20679dc9062c",
+    "url": "/status-localize/static/media/korea.c5c7b09c.png"
+  },
+  {
+    "revision": "6345ed4cee6b184a98f87e04d97bc5c3",
+    "url": "/status-localize/static/media/latin_america.6345ed4c.png"
+  },
+  {
+    "revision": "b8735962154cec9fe4e83479f6bc3d30",
+    "url": "/status-localize/static/media/logo.b8735962.png"
+  },
+  {
+    "revision": "18ef14ce08f5d30406b27ddba5230ec5",
+    "url": "/status-localize/static/media/lokalise1.18ef14ce.png"
+  },
+  {
+    "revision": "4cc6b0c7fd619c20b656615d26a0b5e4",
+    "url": "/status-localize/static/media/lokalise2.4cc6b0c7.png"
+  },
+  {
+    "revision": "662eec419541542f62cf2c9d44f13006",
+    "url": "/status-localize/static/media/lokalise3.662eec41.png"
+  },
+  {
+    "revision": "769f2aeec1f8fba3a6e665587be434c9",
+    "url": "/status-localize/static/media/russia.769f2aee.svg"
+  },
+  {
+    "revision": "2983be30b37be12d3b143a4caa0e07cc",
+    "url": "/status-localize/static/media/spain.2983be30.png"
+  },
+  {
+    "revision": "92175f6805ea39ad79652b03350ae82d",
+    "url": "/status-localize/static/media/world-map.92175f68.png"
+  }
+]);
