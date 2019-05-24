8c40c16119cc8008e42125accf49c1a42bc3dab7
Jenkins <jenkins@status.im>
2019-05-24 12:20:20 +0000

    Updates

---
+++ precache-manifest.988867fc085aaee925246771617f99b3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eab9d7cca466812923d586b121396b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87154ce1a6f390d8e406",
+    "url": "/static/css/main.97ddc340.chunk.css"
+  },
+  {
+    "revision": "b4bbf11130963fc96870",
+    "url": "/static/js/2.c6ef02f0.chunk.js"
+  },
+  {
+    "revision": "87154ce1a6f390d8e406",
+    "url": "/static/js/main.081bad88.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c5c7b09c827d98586b1e20679dc9062c",
+    "url": "/static/media/korea.c5c7b09c.png"
+  },
+  {
+    "revision": "6345ed4cee6b184a98f87e04d97bc5c3",
+    "url": "/static/media/latin_america.6345ed4c.png"
+  },
+  {
+    "revision": "b8735962154cec9fe4e83479f6bc3d30",
+    "url": "/static/media/logo.b8735962.png"
+  },
+  {
+    "revision": "18ef14ce08f5d30406b27ddba5230ec5",
+    "url": "/static/media/lokalise1.18ef14ce.png"
+  },
+  {
+    "revision": "4cc6b0c7fd619c20b656615d26a0b5e4",
+    "url": "/static/media/lokalise2.4cc6b0c7.png"
+  },
+  {
+    "revision": "662eec419541542f62cf2c9d44f13006",
+    "url": "/static/media/lokalise3.662eec41.png"
+  },
+  {
+    "revision": "769f2aeec1f8fba3a6e665587be434c9",
+    "url": "/static/media/russia.769f2aee.svg"
+  },
+  {
+    "revision": "2983be30b37be12d3b143a4caa0e07cc",
+    "url": "/static/media/spain.2983be30.png"
+  },
+  {
+    "revision": "92175f6805ea39ad79652b03350ae82d",
+    "url": "/static/media/world-map.92175f68.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/status-im/translate.status.im/precache-manifest.603dba8c1c099bcecc0595486d3341e7.js"
+  "/precache-manifest.988867fc085aaee925246771617f99b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/status-im/translate.status.im/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
