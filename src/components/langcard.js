963f4afb66f94a4139b08c89fee1b935145d7b03
jinhojang6 <jinho5051@gmail.com>
2020-05-04 02:04:22 +0900

    Refactor Langcard.js with hooks

--- src/components/langcard.js
+++ src/components/langcard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Line } from 'rc-progress';
 
@@ -7,63 +7,44 @@ const getProgressForLang = (progressLang, lang) => {
   return rval === undefined ? 0 : (rval['progress'] || 0)
 }
 
-class LangCard extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            cardItem: null,
-            progress_by_lang: null,
-        }
+const LangCard = (props) => {
+
+    const [cardItem, setCardItem] = useState(null);
+    const [progressByLang, setProgressByLang] = useState(null);
+
+    const setCardStateOnProps = () => {
+        const { card, progress } = props
+        setCardItem(card)
+        setProgressByLang(progress)
+    }
+
+    useEffect(() => {
+        setCardStateOnProps()
+    })
+
+    let dapp_progress = 0
+    let website_progress = 0
+
+    if ((typeof(progressByLang) !== 'undefined') && (progressByLang !== null) && (progressByLang.length !== 0)) {
+        dapp_progress = getProgressForLang(progressByLang[0]['Status DApp'], cardItem.code)
+        website_progress = getProgressForLang(progressByLang[1]['Status.im Website'], cardItem.code)
     }
     
-    handleRemoveContentButton(event) {
-        if (this.props.didHandleRemove) {
-            this.props.didHandleRemove(this.props.card)
-        }
-    }
-
-    setCardStateOnProps() {
-        const { card, progress } = this.props
-        this.setState({
-            cardItem: card,
-            progress_by_lang: progress
-        })
-    }
-
-    componentDidMount() {
-        this.setCardStateOnProps()
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshop) {
-        if (this.props !== prevProps) {
-            this.setCardStateOnProps()
-        }
-    }
-
-    render(){
-        const { cardItem, progress_by_lang } = this.state
-        let dapp_progress = 0
-        let website_progress = 0
-        if ((typeof(progress_by_lang) !== 'undefined') && (progress_by_lang !== null) && (progress_by_lang.length !== 0)) {
-            dapp_progress = getProgressForLang(progress_by_lang[0]['Status DApp'], cardItem.code)
-            website_progress = getProgressForLang(progress_by_lang[1]['Status.im Website'], cardItem.code)
-        }
-        return (
-            <div className="card align-center">
-                {cardItem !== null
-                    ? 
-                    <div className="padding-top-20">
-                        <p className="language-title">{cardItem.lang_en}</p>
-                        <p className="language-name">{cardItem.lang}</p>
-                        <p><a href={cardItem.app} target="_blank" rel="noopener noreferrer"><FormattedMessage id="main.app-application" defaultMessage="Status app translation" /></a></p>
-                        <Line percent={dapp_progress} strokeWidth="3" trailWidth="3" strokeColor="#2db7f5" style={{ width: '50%'}} /> <span style={{ marginLeft: '20px', fontFamily: 'Roboto'}}>{dapp_progress}%</span>
-                        <p><a href={cardItem.website} target="_blank" rel="noopener noreferrer"><FormattedMessage id="main.website-application" defaultMessage="Status.im website translation" /></a></p>
-                        <Line percent={website_progress} strokeWidth="3" trailWidth="3" strokeColor="#2db7f5" style={{ width: '50%'}} /> <span style={{ marginLeft: '20px', fontFamily: 'Roboto'}}>{website_progress}%</span>
-                    </div>
-                    : ''}
-            </div>
-        );
-    }
+    return (
+        <div className="card align-center">
+            {cardItem !== null
+                ? 
+                <div className="padding-top-20">
+                    <p className="language-title">{cardItem.lang_en}</p>
+                    <p className="language-name">{cardItem.lang}</p>
+                    <p><a href={cardItem.app} target="_blank" rel="noopener noreferrer"><FormattedMessage id="main.app-application" defaultMessage="Status app translation" /></a></p>
+                    <Line percent={dapp_progress} strokeWidth="3" trailWidth="3" strokeColor="#2db7f5" style={{ width: '50%'}} /> <span style={{ marginLeft: '20px', fontFamily: 'Roboto'}}>{dapp_progress}%</span>
+                    <p><a href={cardItem.website} target="_blank" rel="noopener noreferrer"><FormattedMessage id="main.website-application" defaultMessage="Status.im website translation" /></a></p>
+                    <Line percent={website_progress} strokeWidth="3" trailWidth="3" strokeColor="#2db7f5" style={{ width: '50%'}} /> <span style={{ marginLeft: '20px', fontFamily: 'Roboto'}}>{website_progress}%</span>
+                </div>
+                : ''}
+        </div>
+    );
 
 }
 
